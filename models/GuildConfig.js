b2a69264ce35cebff08992acca7b7e4e603b1cb0
EL Yaw! <kesyayaw@gmail.com>
2026-07-12 16:38:25 +0700

    Delete models directory

--- models/GuildConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const guildConfigSchema = new mongoose.Schema({
-  guildId: { type: String, required: true, unique: true },
-  voiceLogChannelId: { type: String, default: null },
-}, { timestamps: true });
-
-module.exports = mongoose.model('GuildConfig', guildConfigSchema);
